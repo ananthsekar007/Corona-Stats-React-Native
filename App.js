58736cc6d66b14ed798ef4f79bcfcc9439d90f3b
Ananth Sekar <ananth.sekar@gogsoon.com>
2020-07-12 22:18:23 +0530

    [Ananth] Implements navigation container and screens

--- App.js
+++ App.js
@@ -7,12 +7,20 @@
  */
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View>
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+            headerShown: false
+        }}>
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 export default App;
