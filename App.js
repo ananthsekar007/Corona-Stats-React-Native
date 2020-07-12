420c2e6c98e7ef58b232b6e85cb73f907f1399db
Ananth Sekar <ananth.sekar@gogsoon.com>
2020-07-12 23:33:53 +0530

    [Ananth] Adds Spplach screen and dummy Home page

--- App.js
+++ App.js
@@ -10,15 +10,19 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Splash from "./src/screens/Splash/Splash"
+import Home from "./src/screens/Home/Home";
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-        <Stack.Navigator screenOptions={{
+        <Stack.Navigator initialRouteName="Splash" screenOptions={{
             headerShown: false
         }}>
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/Splash/Splash.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ActivityIndicator,
+} from 'react-native';
+import image from './../../../assets/images/app_logo.png';
+
+const {width: WIDTH, height: HEIGHT} = Dimensions.get('window');
+
+export default class Splash extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+    };
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      isLoading: true,
+    });
+    setTimeout(() => {
+      this.props.navigation.navigate('Home');
+      this.setState({
+        isLoading: false,
+      });
+    }, 2000);
+  };
+
+  render() {
+    return (
+      <View
+        style={{
+          backgroundColor: '#ECEBEB ',
+          flex: 1,
+        }}>
+        <View style={styles.imageContainer}>
+          <Image source={image} style={styles.image} />
+        </View>
+        <Text style={styles.appText}>Corona Stats</Text>
+        <ActivityIndicator
+          animating={this.state.isLoading}
+          color="#959595"
+          style={{
+            marginTop: 10,
+          }}
+        />
+        <View style={styles.bottomTextContainer}>
+          <Text>© 2020 Corona Stats</Text>
+          <Text style={styles.devText}>App by Ananth</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    justifyContent: 'center',
+    width: 120,
+    height: 120,
+    alignSelf: 'center',
+    marginTop: HEIGHT / 4,
+  },
+  image: {
+    flex: 1,
+    maxWidth: '100%',
+  },
+  appText: {
+    fontFamily: 'CarterOne-Regular',
+    fontSize: 30,
+    alignSelf: 'center',
+    marginTop: 40,
+  },
+  bottomTextContainer: {
+    position: 'absolute',
+    bottom: 10,
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  devText: {
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
